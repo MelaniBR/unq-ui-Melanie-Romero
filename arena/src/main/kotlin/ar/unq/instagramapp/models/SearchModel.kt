f8dd852ae751c9742300b63c38a16fa3a8490cf6
mlucas94 <lucasmoron0512@gmail.com>
2020-09-10 01:52:04 -0300

    SearchModel y SearchView agregados

--- arena/src/main/kotlin/ar/unq/instagramapp/models/SearchModel.kt
+++ arena/src/main/kotlin/ar/unq/instagramapp/models/SearchModel.kt
@@ -3,21 +3,59 @@ package ar.unq.instagramapp.models;
 import org.unq.ui.model.InstagramSystem
 import org.unq.ui.model.Post
 import org.uqbar.commons.model.annotations.Observable
+import scala.collection.mutable.`MutableList$`
 
 @Observable
  class SearchModel(
     val instagramSystem: InstagramSystem,
+    var userId : String = "",
     var searchInput: String = "",
-    var selectedPost : String = "",
-    var searchResults : List<Post> = listOf()
+    var searchResults : MutableList<PostModel> = mutableListOf(),
+    //var searchResults : List<Post> = listOf(),
+    var selectedPost : PostModel = searchResults[0]
+) {
 
-)
+    //Se encarga de transformar los posts en post observables
+    fun postList(posts : List<Post>) : MutableList<PostModel> {
+       var result : MutableList<PostModel> = mutableListOf()
+       for (post in posts) {
+           result.add(PostModel(post))
+       }
+       return result
+   }
+
+    fun loadMyPosts() {
+        searchResults = postList(instagramSystem.searchByUserId(userId))
+    }
+
+    //Implementacion si la de arriba no llega a funcar
+    /*
+    fun postLists(posts : List<Post> : MutableList<PostModel>) {
+        var result : MutableList<PostModel> = mutableListOf()
+        for (post in posts) {
+            var newPostM = PostModel()
+            newPostM.postId = post.id
+            newPostM.postDescription = post.description
+            newPostM.postLandscape = post.landscape
+            newPostM.postPortrait = post.portrait
+            newPostM.postUser = post.user.name
+            result.add(newPostM)
+        }
+    }
+    */
+}
+
 
 @Observable
-class PostModel(
-    var postId : String = "",
-    var postUser : String = "",
-    var postLandscape : String = "",
-    var postPortrait : String = "",
-    var postDescription : String = ""
-)
+class PostModel( private val post: Post) {
+
+    var postId : String = post.id
+    var postUser : String = post.user.name
+    var postLandscape : String = post.landscape
+    var postPortrait : String = post.portrait
+    var postDescription : String = post.description
+
+}
+
+
+
---
+++ arena/src/main/kotlin/ar/unq/instagramapp/viewModels/SearchWindow.kt
@@ -0,0 +1,92 @@
+package ar.unq.instagramapp.viewModels;
+
+import ar.unq.instagramapp.IObserverInstagramApp
+import ar.unq.instagramapp.InstagramApp
+import ar.unq.instagramapp.models.PostModel
+import ar.unq.instagramapp.models.SearchModel
+import org.uqbar.arena.kotlin.extensions.*
+import org.uqbar.arena.layout.HorizontalLayout
+import org.uqbar.arena.widgets.Button
+import org.uqbar.arena.widgets.Label
+import org.uqbar.arena.widgets.Panel
+import org.uqbar.arena.widgets.TextBox
+import org.uqbar.arena.windows.Window
+import org.uqbar.arena.windows.WindowOwner
+
+class SearchWindow(owner: WindowOwner, model: SearchModel) : Window<SearchModel>(owner, model) {
+
+    override fun createContents(mainPanel: Panel) {
+        title = "Mis Posts"
+        this.setMinWidth(500)
+        this.setMinHeight(300)
+
+        Label(mainPanel) with{
+            text = "Posts"
+        }
+
+        var searchInputPanel = Panel(mainPanel)
+        searchInputPanel.layout = HorizontalLayout()
+
+        Label(searchInputPanel) with {
+            text = "Search: "
+        }
+        TextBox(searchInputPanel) with {
+            bindTo("searchInput")
+        }
+        Button(searchInputPanel) with {
+            caption = "Search"
+            //Probablemente haya que cambiar esto
+            onClick { modelObject.instagramSystem.searchByTag(modelObject.searchInput) }
+        }
+        //Tabla
+        table<PostModel>(mainPanel) {
+            bindItemsTo("searchResults")
+            //bindSelectionTo("selected")
+            visibleRows = 5
+            column {
+                title = "#"
+                bindContentsTo("postId")
+            }
+            column {
+                title = "Landscape"
+                bindContentsTo("postLandscape")
+            }
+            column {
+                title = "Portrait"
+                bindContentsTo("postPortrait")
+            }
+
+            var buttonBar = Panel(mainPanel)
+            buttonBar.layout = HorizontalLayout()
+            Button(buttonBar) with{
+                caption = "Add new post"
+                onClick { showEditPostWindow() }
+                bindVisibleTo("loginOk")
+            }
+            Button(buttonBar) with{
+                caption = "Edit Post"
+                onClick { showNewPostWindow() }
+                bindVisibleTo("loginOk")
+            }
+            Button(buttonBar) with{
+                caption = "Delete Post"
+                onClick { showDeletePostWindow() }
+                bindVisibleTo("loginOk")
+            }
+        }
+    }
+
+    private fun showEditPostWindow(){
+        //TODO crear ventana de edit post y abrirla desde este metodo
+    }
+
+    private fun showDeletePostWindow(){
+        //TODO crear ventana de delete post y abrirla desde este metodo
+    }
+
+    private fun showNewPostWindow(){
+        //TODO crear ventana de new post y abrirla desde este metodo
+    }
+
+
+}
