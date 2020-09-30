d47ced05bf205b3cc3bcc680f803a318f59de228
mlucas94 <lucasmoron0512@gmail.com>
2020-09-30 19:56:39 -0300

    API register implementado.

--- api/src/main/kotlin/ar/unq/controllers/RegisterController.kt
+++ api/src/main/kotlin/ar/unq/controllers/RegisterController.kt
@@ -1,11 +1,25 @@
 package ar.unq.controllers
 
+import ar.unq.utils.requests.RegisterRequest
+import ar.unq.utils.responses.ErrorResponse
 import io.javalin.http.Context
 import org.unq.ui.model.InstagramSystem
+import org.unq.ui.model.UsedEmail
 
 
 class RegisterController(val instagramSystem : InstagramSystem) {
     fun post(ctx: Context){
+        val newUserData = ctx.body<RegisterRequest>()
+        try{
+            instagramSystem.register(
+                newUserData.name,
+                newUserData.email,
+                newUserData.password,
+                newUserData.image)
+
+        }catch(e : UsedEmail){
+            ctx.status(400).json(ErrorResponse("Email already in use"))
+        }
 
     }
 }
