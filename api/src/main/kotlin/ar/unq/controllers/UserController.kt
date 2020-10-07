59d53eee97bfb3ed28986c686fc1cf3c53c258b1
mlucas94 <lucasmoron0512@gmail.com>
2020-10-07 19:07:33 -0300

    Refactor UserController

--- api/src/main/kotlin/ar/unq/controllers/UserController.kt
+++ api/src/main/kotlin/ar/unq/controllers/UserController.kt
@@ -1,13 +1,10 @@
 package ar.unq.controllers
 
-import ar.unq.token.TokenController
 import ar.unq.utils.responses.ErrorResponse
 import ar.unq.utils.responses.OkResponse
 import ar.unq.utils.responses.UserResponses.GetUserByIdResponse
 import ar.unq.utils.responses.UserResponses.GetUserResponse
 import io.javalin.http.Context
-import io.javalin.http.UnauthorizedResponse
-import javalinjwt.JavalinJWT
 import org.unq.ui.model.InstagramSystem
 import org.unq.ui.model.NotFound
 import org.unq.ui.model.Post
@@ -15,8 +12,6 @@ import org.unq.ui.model.User
 
 
 class UserController(val instagramSystem : InstagramSystem) {
-
-    //val tokenController = TokenController()
 
     fun get(ctx: Context){
 
@@ -29,8 +24,6 @@ class UserController(val instagramSystem : InstagramSystem) {
         } catch (e: NotFound) {
             //TODO: aca devolver un notfound cuando el usuario no existe
             ctx.status( 404 ).json(ErrorResponse("El usuario no existe"))
-        } catch (e : Exception) {
-            ctx.status(500).json(ErrorResponse("Error interno"))
         }
 
         val userTimeline = instagramSystem.timeline(userId)
@@ -46,14 +39,11 @@ class UserController(val instagramSystem : InstagramSystem) {
 
         try {
             user = instagramSystem.getUser(id)
+            userPosts = instagramSystem.searchByUserId(id)
         }catch(e : NotFound) {
             ctx.status(404).json(ErrorResponse("No se encontro usuario con "+ id))
         }
-        try {
-            userPosts = instagramSystem.searchByUserId(id)
-        }catch (e : Exception) {
-            ctx.status(500).json(ErrorResponse("Error interno"))
-        }
+
 
         val userDTO = GetUserByIdResponse(user!!, userPosts)
         ctx.status(200).json(userDTO)
@@ -64,10 +54,10 @@ class UserController(val instagramSystem : InstagramSystem) {
         val loggedUserId : String = ctx.attribute<String>("userId").toString()
         try {
             instagramSystem.updateFollower(loggedUserId, followedUserId)
-            ctx.status(200).json(OkResponse())
         }catch (e: NotFound) {
             ctx.status(404).json(ErrorResponse("No existe el usuario con id " + followedUserId))
         }
+        ctx.status(200).json(OkResponse())
 
     }
 }
