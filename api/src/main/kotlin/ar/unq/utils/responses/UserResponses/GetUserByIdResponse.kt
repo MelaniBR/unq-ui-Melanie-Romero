22e3271649fa32016c6711661717500bcd033a49
mlucas94 <lucasmoron0512@gmail.com>
2020-10-07 18:04:55 -0300

    API GetUserId internal error

--- api/src/main/kotlin/ar/unq/utils/responses/UserResponses/GetUserByIdResponse.kt
+++ api/src/main/kotlin/ar/unq/utils/responses/UserResponses/GetUserByIdResponse.kt
@@ -3,13 +3,16 @@ package ar.unq.utils.responses.UserResponses;
 import org.unq.ui.model.Post
 import org.unq.ui.model.User
 
+data class PostOnlyID (val postId : String)
+}
+
 class GetUserByIdResponse (
     user : User,
     userPosts : List<Post>
 ) {
     val name : String = user.name
     val image : String = user.image
-    val posts : List<PostResponse> = userPosts.map { PostResponse ( it ) }
+    val posts : List<PostOnlyID> = userPosts.map { PostOnlyID ( it.id) }
 
     /*
     fun setPosts(ps : List<Post>) : MutableList<Post> {
