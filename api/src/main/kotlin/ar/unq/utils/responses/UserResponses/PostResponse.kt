22b90043a4f22bab83acd8153b7e3c90be31fb23
MelaniBR <melaniromero1999@hotmail.com>
2020-10-07 16:49:04 -0300

    se agrego TimeLineResponseWithComments

--- api/src/main/kotlin/ar/unq/utils/responses/UserResponses/PostResponse.kt
+++
@@ -1,9 +0,0 @@
-package ar.unq.utils.responses.UserResponses
-
-import org.unq.ui.model.Post
-
-class PostResponse(val post: Post){
-    val posts = TimeLineResponse(post)
-    val comments :List<CommentResponse> = post.comments.map{ CommentResponse(it) }
-
-}
