cc4324ad838c8f33fb53445f7bd3a43a15963ac6
Pablo G. Gonzalez <pablogonzalez.estudio@gmail.com>
2020-11-16 16:00:46 -0300

    profile

--- web/instagram-web/src/components/Profile.jsx
+++ web/instagram-web/src/components/Profile.jsx
@@ -1,11 +1,11 @@
 import React, { useEffect, useState } from 'react';
+import { NavLink } from 'react-router-dom';
 import { userById } from './Api';
 
 export const Profile = (props) => {
 
-  const[data, setData] = useState({});
+  const[data, setData] = useState({posts: []});
 
-  // In a function component:
   useEffect(() => {
     userById(props.auth.id, props.auth.token)
     .then(response => {
@@ -21,9 +21,15 @@ export const Profile = (props) => {
     <>
       <h1>{props.auth.name}</h1>
       <img src={data.image} />
+      <div class="row">
       {data.posts.map(post => (
-        <li key={post.id}>{post.description}</li>
+        <div class="col-6 col-md-4 col-lg-3 mb-4">
+          <div class="card mx-auto text-center">
+            <NavLink to={`/post/${post.id}`}><img class="card-img-top" src={post.landscape} alt="Sample Title"/></NavLink>
+          </div>
+        </div>
       ))}
+      </div>
     </>
     
   )
