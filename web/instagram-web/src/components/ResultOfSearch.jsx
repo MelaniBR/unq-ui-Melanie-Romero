3caa3c965eef3b9ec71070afddab6d34edcd0193
MelaniBR <melaniromero1999@hotmail.com>
2020-11-18 12:43:23 -0300

    subo ResultOfSearch

--- web/instagram-web/src/components/ResultOfSearch.jsx
+++ web/instagram-web/src/components/ResultOfSearch.jsx
@@ -1,7 +1,6 @@
 import React from 'react';
 import ResultUser from './ResultUser';
-/// recibiria el resultado para el manejo del resultado
-// solo de usario
+
 export default function ResultOfSearch({results}) {
     if(results){
         return(
