2738b8c35ee2a2047717851d925a36d8d804b7db
Pablo G. Gonzalez <pablogonzalez.estudio@gmail.com>
2020-11-18 19:15:21 -0300

    id en follower

--- api/src/main/kotlin/ar/unq/utils/responses/UserResponses/FollowerResponse.kt
+++ api/src/main/kotlin/ar/unq/utils/responses/UserResponses/FollowerResponse.kt
@@ -1,3 +1,3 @@
 package ar.unq.utils.responses.UserResponses
 
-data class FollowerResponse(val name : String, val image:String ){}
+data class FollowerResponse(val name : String, val image:String, val id: String ){}
--- api/src/main/kotlin/ar/unq/utils/responses/UserResponses/GetUserResponse.kt
+++ api/src/main/kotlin/ar/unq/utils/responses/UserResponses/GetUserResponse.kt
@@ -10,5 +10,5 @@ data class GetUserResponse(
     private val userFollowers: List<User>
 ) : UserResponse(user){
     val timeline : List<PostResponse> = userTimeline.map { PostResponse( it, user.id ) }
-    val followers : List<FollowerResponse> = userFollowers.map { FollowerResponse( it.name, it.image )}
+    val followers : List<FollowerResponse> = userFollowers.map { FollowerResponse( it.name, it.image, it.id )}
 }
