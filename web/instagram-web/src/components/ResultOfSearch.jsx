6d06f538c65d367e1747558058aac88ba0abe45f
MelaniBR <melaniromero1999@hotmail.com>
2020-11-18 10:50:12 -0300

    Ya anda busqueda solo para Usuarios

--- web/instagram-web/src/components/ResultOfSearch.jsx
+++ web/instagram-web/src/components/ResultOfSearch.jsx
@@ -3,21 +3,25 @@ import ResultUser from './ResultUser';
 /// recibiria el resultado para el manejo del resultado
 // solo de usario
 export default function ResultOfSearch({results}) {
-    if(!results){
-        return(
-            <h2>
-                Sin Resultados
-            </h2>
-        );
-    } else {
+    if(results){
         return(
             <div className="container-fluid">
                 <div className="row">
-                    {results.map (result => (
-                        <ResultUser content={result} />
+                    {results.content.map(result => (
+                        <div key={result.id} className="poster" >
+                            <ResultUser content={result}></ResultUser>
+                        </div>
                     ))}
                 </div>
             </div>
+        );
+    } else {
+        return(
+
+            <h2>
+                Sin Resultados
+            </h2>
+
         );
     }
 
--- web/instagram-web/src/components/Search.jsx
+++ web/instagram-web/src/components/Search.jsx
@@ -5,32 +5,26 @@ import ResultOfSearch from './ResultOfSearch';
 
 const Search = (props) => {
 
-    const [content, setContent] = useState([]);
-    const [result, setResult] = useState([]);
+    const [data, setData] = useState();
+    const [result, setResult] = useState();
 
     const useEffect=() => {
-        search(content,props.auth.token).then(res => {
+        search(data,props.auth.token).then(res => {
             setResult(res.data);
         })
+        console.log(result)
     }
 
     return (<div className="search-page">
 
                 <input className="form-control mr-sm-2"  placeholder="Search" aria-label="Search"
-                       value={content}
-                       onChange={event => setContent(event.target.value)}  />
+                       value={data}
+                       onChange={event => setData(event.target.value)}  />
                 <button className="btn btn-primary my-2 my-sm-0" type="submit" onClick={useEffect} >Search</button>
+        <div className="row">
+            <ResultOfSearch results={result}></ResultOfSearch>
 
-
-
-            <h2>
-
-                <h2>
-                    Resultado de la Búsqueda : {content}
-                </h2>
-                <ResultOfSearch results={result} />
-
-            </h2>
+        </div>
         </div>
     );
 }
