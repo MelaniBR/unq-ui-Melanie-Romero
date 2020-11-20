ae33178a6f2bdd393976a731dd9fded8f2e86d08
MelaniBR <melaniromero1999@hotmail.com>
2020-11-19 23:01:36 -0300

    Search completo

--- web/instagram-web/src/components/ResultUser.jsx
+++ web/instagram-web/src/components/ResultUser.jsx
@@ -1,26 +1,22 @@
 import React from 'react';
+import { Link, NavLink } from 'react-router-dom';
 export default function ResultOfSearch({results}) {
 
     // tiene que quedar en el mismo reglon
     return (
-        <div >
+        <div className="my-3 p-3 bg-white rounded shadow-sm">
+            <h6 className="border-bottom border-gray pb-2 mb-0">Search by user result</h6>
+            {results && results.map(result => (
 
-            <div className="card-deck align-content-center card-columns pt-5" >
-                {results && results.map(result => (
-                    <div key={result.id} className="mx-auto" >
-                        <a href={`/user/${result.id}`} >
-                            <p >
-                            <img className="border border-primary rounded-circle mr-2"
-                                 style={{width: "80px", height: "80px"}} src={result.image} align="left"/>
+                <div className="media text-muted pt-3">
+                    <img className="border border-primary rounded-circle mr-2"
+                         style={{width: "70px", height: "70px"}} src={result.image} align="left"/>
 
-                            {result.name}
-
-                            </p>
-                        </a>
-                    </div>
-                ))}
-            </div>
-
+                    <p className="media-body pt-3 pb-3 mb-0 big lh-125 border-bottom border-gray">
+                        <strong className="d-block text-gray-dark"> <Link to={`/user/${result.id}`}>{result.name}</Link></strong>
+                    </p>
+                </div>
+            ))}
         </div>
     );
 }
