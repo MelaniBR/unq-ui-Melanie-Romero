05011ef29384fde8af22f928be90c3733cfdfc10
mlucas94 <lucasmoron0512@gmail.com>
2020-11-19 23:35:14 -0300

    estilo like button

--- web/instagram-web/src/components/Post.jsx
+++ web/instagram-web/src/components/Post.jsx
@@ -41,7 +41,7 @@ const Post = (props) => {
 
         like( id, props.auth.token )
             .then(response => {
-                setPostData({...postData, likes: response.data.likes.length});
+                setPostData({...postData, likes: response.data.likes.length, liked: response.data.like});
             })
             .catch(error => {
 
@@ -53,7 +53,7 @@ const Post = (props) => {
         Swal.fire({
             title: 'New comment',
             html: `<input type="text" id="commentText" class="form-control" placeholder="your comment here">`,
-            confirmButtonText: 'comment',
+            confirmButtonText: 'Confirm',
             preConfirm: () => {
                 const commentText = Swal.getPopup().querySelector('#commentText').value
                 if(!commentText || !commentText.replace(/\s/g, '')) {
@@ -82,12 +82,22 @@ const Post = (props) => {
             <div className="card-body">
                 <div className="card-text">
                     <div className= "card-likes p-2"> 
-                        <button onClick={handleLikeClick} >&hearts;</button>
-                        <>{ postData.likes } <b>likes</b></>
+                      
+                      { postData.liked ? 
+                        <svg width="1em" height="1em" viewBox="0 0 16 16" className="bi bi-suit-heart-fill" fill="currentColor" xmlns="http://www.w3.org/2000/svg">
+                          <path d="M4 1c2.21 0 4 1.755 4 3.92C8 2.755 9.79 1 12 1s4 1.755 4 3.92c0 3.263-3.234 4.414-7.608 9.608a.513.513 0 0 1-.784 0C3.234 9.334 0 8.183 0 4.92 0 2.755 1.79 1 4 1z"/>
+                        </svg>                    
+                      : 
+                        <svg width="1em" height="1em" viewBox="0 0 16 16" className="bi bi-heart" fill="currentColor" xmlns="http://www.w3.org/2000/svg">
+                          <path fillRule="evenodd" d="M8 2.748l-.717-.737C5.6.281 2.514.878 1.4 3.053c-.523 1.023-.641 2.5.314 4.385.92 1.815 2.834 3.989 6.286 6.357 3.452-2.368 5.365-4.542 6.286-6.357.955-1.886.838-3.362.314-4.385C13.486.878 10.4.28 8.717 2.01L8 2.748zM8 15C-7.333 4.868 3.279-3.04 7.824 1.143c.06.055.119.112.176.171a3.12 3.12 0 0 1 .176-.17C12.72-3.042 23.333 4.867 8 15z"/>
+                        </svg>
+                      }
+                      <span className="ml-2">{postData.likes} { postData.likes == 1 ? "like" : "likes"} </span>
                     </div>
-                    <form className="p-2" onSubmit={handleAddComment}>
-                                <button type="submit" className="btn-comment-submit btn-primary rounded">Comment</button>
-                        </form>
+
+                    <button className="btn btn-primary m-2" onClick={handleLikeClick} >{ postData.liked ? "Unlike" : "Like"} </button>
+                    <button onClick={handleAddComment} className="btn btn-primary m-2">Comment</button>
+
                     <div className= "card-comments">
                         <ul>
                             {postData.comments.map((comment)=>
--- web/instagram-web/src/components/PostItem.jsx
+++ web/instagram-web/src/components/PostItem.jsx
@@ -6,6 +6,7 @@ const PostItem = (props) => {
 
     const portrait = props.post.portrait;
     const[likes, setLikes] = useState(props.post.likes.length);
+    const[liked, setLiked] = useState(props.post.liked)
     const description = props.post.description;
     const userName = props.userName;
     const userImage = props.userImage;
@@ -17,6 +18,7 @@ const PostItem = (props) => {
         like( props.post.id, props.auth.token )
             .then(response => {
                 setLikes(response.data.likes.length);
+                setLiked(response.data.liked)
             })
             .catch(error => {
             })
@@ -35,10 +37,22 @@ const PostItem = (props) => {
             </div>
             <div className= "card-body">
                 <div className= "card-text">
-                    <div className= "card-likes mb-2"> 
-                        <button onClick={handleLikeClick} >&hearts;</button>
-                        <>{ likes } <b>likes</b></>
+                <div className= "card-likes p-2"> 
+                      
+                      { liked ? 
+                        <svg width="1em" height="1em" viewBox="0 0 16 16" className="bi bi-suit-heart-fill" fill="currentColor" xmlns="http://www.w3.org/2000/svg">
+                          <path d="M4 1c2.21 0 4 1.755 4 3.92C8 2.755 9.79 1 12 1s4 1.755 4 3.92c0 3.263-3.234 4.414-7.608 9.608a.513.513 0 0 1-.784 0C3.234 9.334 0 8.183 0 4.92 0 2.755 1.79 1 4 1z"/>
+                        </svg>                    
+                      : 
+                        <svg width="1em" height="1em" viewBox="0 0 16 16" className="bi bi-heart" fill="currentColor" xmlns="http://www.w3.org/2000/svg">
+                          <path fillRule="evenodd" d="M8 2.748l-.717-.737C5.6.281 2.514.878 1.4 3.053c-.523 1.023-.641 2.5.314 4.385.92 1.815 2.834 3.989 6.286 6.357 3.452-2.368 5.365-4.542 6.286-6.357.955-1.886.838-3.362.314-4.385C13.486.878 10.4.28 8.717 2.01L8 2.748zM8 15C-7.333 4.868 3.279-3.04 7.824 1.143c.06.055.119.112.176.171a3.12 3.12 0 0 1 .176-.17C12.72-3.042 23.333 4.867 8 15z"/>
+                        </svg>
+                      }
+                      <span className="ml-2">{likes} { likes == 1 ? "like" : "likes"} </span>
                     </div>
+                    
+                    <button className="btn btn-primary m-1" onClick={handleLikeClick} >{ liked ? "Unlike" : "Like"} </button>
+                    
                     <div className= "card-description p-2">
                         <p>{ description }</p>
                     </div>
