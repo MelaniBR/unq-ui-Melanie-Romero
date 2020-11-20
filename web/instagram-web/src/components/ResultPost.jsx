2ffb2fa7ea0deaaea9343c78a499d8893caad21b
MelaniBR <melaniromero1999@hotmail.com>
2020-11-19 21:00:55 -0300

    Search terminado

--- web/instagram-web/src/components/ResultPost.jsx
+++ web/instagram-web/src/components/ResultPost.jsx
@@ -7,7 +7,7 @@ export default function ResultPost({results}) {
             <div className="card-deck">
                 {results.map(result => (
 
-                    <div key={result.id}  className="card" style={{width:"100px", height:"100px"}}>
+                    <div key={result.id}  className="card" className="col-6 col-md-4 col-lg-3 mb-4">
                         <a href={`/post/${result.id}`} >
                             <img  className="card-img-top"  src={result.portrait}/>
                         </a>
