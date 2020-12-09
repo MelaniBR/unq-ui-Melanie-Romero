bb98094d292fae8fd3315c8dffe9c76d0e3bdc4f
MelaniBR <melaniromero1999@hotmail.com>
2020-12-09 13:21:06 -0300

    Agrego error en espacios en blancos

--- api/src/main/kotlin/ar/unq/controllers/PostController.kt
+++ api/src/main/kotlin/ar/unq/controllers/PostController.kt
@@ -12,47 +12,52 @@ import org.unq.ui.model.InstagramSystem
 import org.unq.ui.model.NotFound
 import org.unq.ui.model.Post
 
-class PostController(val instagramSystem : InstagramSystem) {
-    fun getById(ctx: Context){
-        var post : Post? = null
+class PostController(val instagramSystem: InstagramSystem) {
+    fun getById(ctx: Context) {
+        var post: Post? = null
         val id = ctx.pathParam("postId")
 
         try {
             post = instagramSystem.getPost(id)
-        }catch(e : NotFound) {
+        } catch (e: NotFound) {
             ctx.status(404).json(ErrorResponse("Not found post with id" + id))
             return
         }
-        val postResponse : PostResponseWithComments = PostResponseWithComments(post!!, getUserId(ctx))
+        val postResponse: PostResponseWithComments = PostResponseWithComments(post!!, getUserId(ctx))
         ctx.status(200).json(postResponse)
     }
 
-    fun toggleLike(ctx: Context){
+    fun toggleLike(ctx: Context) {
         val userId = getUserId(ctx)
         val idPost = ctx.pathParam("postId")
         try {
-            instagramSystem.updateLike(idPost,userId)
+            instagramSystem.updateLike(idPost, userId)
             val post = instagramSystem.getPost(idPost)
             ctx.status(200).json(PostLikesResponse(post, userId))
-        }catch(e : NotFound) {
+        } catch (e: NotFound) {
             ctx.status(404).json(ErrorResponse("Not found post with id" + idPost))
         }
     }
 
-    fun addComment(ctx: Context){
+    fun addComment(ctx: Context) {
         val userId = getUserId(ctx)
         val idPost = ctx.pathParam("postId")
-        val comment =  ctx.body<DraftComment>()
+        val comment = ctx.body<DraftComment>()
+        if (comment.body.isNullOrEmpty()){
+            throw BadRequestResponse("empty field")
+            }
+        this.addCommentThatIsNotEmpty(idPost,userId,comment,ctx)
+    }
+    private fun addCommentThatIsNotEmpty(idPost:String,userId:String,comment:DraftComment,ctx: Context){
         try {
-            instagramSystem.addComment(idPost,userId,comment)
+            instagramSystem.addComment(idPost, userId, comment)
 
-            ctx.status(200).json(instagramSystem.getPost(idPost).comments.map{ CommentResponse(it) })
-        }catch(e : NotFound) {
+            ctx.status(200).json(instagramSystem.getPost(idPost).comments.map { CommentResponse(it) })
+        } catch (e: NotFound) {
             ctx.status(404).json(ErrorResponse("Not found post with id" + idPost))
         }
-
-
     }
+
     private fun getUserId(ctx: Context): String {
         return ctx.attribute<String>("userId") ?: throw BadRequestResponse("Not found user")
     }
--- api/src/main/kotlin/ar/unq/controllers/RegisterController.kt
+++ api/src/main/kotlin/ar/unq/controllers/RegisterController.kt
@@ -5,27 +5,42 @@ import ar.unq.utils.requests.RegisterRequest
 import ar.unq.utils.responses.ErrorResponse
 import ar.unq.utils.responses.OkResponse
 import ar.unq.utils.responses.UserResponses.UserResponse
+import io.javalin.http.BadRequestResponse
 import io.javalin.http.Context
 import org.unq.ui.model.InstagramSystem
 import org.unq.ui.model.UsedEmail
 
 
-class RegisterController(val instagramSystem : InstagramSystem) {
-    fun post(ctx: Context){
+class RegisterController(val instagramSystem: InstagramSystem) {
+    fun post(ctx: Context) {
         val newUserData = ctx.body<RegisterRequest>()
-        try{
+        if ( isNullOrEmptyData(newUserData) ){
+            throw BadRequestResponse("empty field")
+        }
+        this.registerUser(ctx ,newUserData);
+    }
+
+    fun isNullOrEmptyData(newUserData: RegisterRequest): Boolean {
+        return newUserData.name.isNullOrEmpty()&&
+                newUserData.email.isNullOrEmpty() &&
+                newUserData.password.isNullOrEmpty();
+    }
+
+    fun registerUser(ctx: Context , newUserData: RegisterRequest) {
+
+        try {
             val user = instagramSystem.register(
                 newUserData.name,
                 newUserData.email,
                 newUserData.password,
-                newUserData.image)
+                newUserData.image
+            )
             ctx.header("Authorization", TokenController().generateToken(user))
             ctx.status(201).json(UserResponse(user))
 
-        }catch(e : UsedEmail){
+        } catch (e: UsedEmail) {
             //409 Conflict Response
             ctx.status(409).json(ErrorResponse("The email is already in use"))
         }
-
     }
 }
